f9ba4e116b79970e460e0538d70a63a6f22cfc85
hwk9407 <hwk__@naver.com>
2025-02-21 19:11:52 +0900

    git init

---
+++ src/main/java/com/hwk9407/bookmanagementassignment/BookManagementAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.hwk9407.bookmanagementassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookManagementAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hwk9407/bookmanagementassignment/BookManagementAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwk9407.bookmanagementassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
