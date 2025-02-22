b8c446a413124adbbf6f70c45bc4da6e0d43834c
hwk9407 <hwk__@naver.com>
2025-02-22 16:26:10 +0900

    [#3]feat: 책 목록 및 상세 조회 Response Dto 에 정적 팩토리 메서드 추가

--- src/main/java/com/hwk9407/bookmanagementassignment/api/book/dto/response/RetrieveAllBooksResponse.java
+++ src/main/java/com/hwk9407/bookmanagementassignment/api/book/dto/response/RetrieveAllBooksResponse.java
@@ -1,12 +1,18 @@
 package com.hwk9407.bookmanagementassignment.api.book.dto.response;
 
-import java.time.LocalDate;
+import com.hwk9407.bookmanagementassignment.domain.book.Book;
+
+import java.util.List;
 
 public record RetrieveAllBooksResponse(
-        String title,
-        String description,
-        String isbn,
-        LocalDate publicationDate,
-        Long authorId
+        List<RetrieveBookResponse> contents
 ) {
+
+    public static RetrieveAllBooksResponse from(List<Book> books) {
+        return new RetrieveAllBooksResponse(
+                books.stream()
+                        .map(RetrieveBookResponse::from)
+                        .toList()
+        );
+    }
 }
---
+++ src/main/java/com/hwk9407/bookmanagementassignment/api/book/dto/response/RetrieveBookResponse.java
@@ -0,0 +1,24 @@
+package com.hwk9407.bookmanagementassignment.api.book.dto.response;
+
+import com.hwk9407.bookmanagementassignment.domain.book.Book;
+
+import java.time.LocalDate;
+
+public record RetrieveBookResponse(
+        String title,
+        String description,
+        String isbn,
+        LocalDate publicationDate,
+        Long authorId
+) {
+
+    public static RetrieveBookResponse from(Book book) {
+        return new RetrieveBookResponse(
+                book.getTitle(),
+                book.getDescription(),
+                book.getIsbn(),
+                book.getPublicationDate(),
+                book.getAuthor().getId()
+        );
+    }
+}
