f8b0b7fb75b7b295fb5221a9f0531f6f86fb14af
hwk9407 <hwk__@naver.com>
2025-02-23 00:27:04 +0900

    [#5]feat: 저자 상세 조회 API 추가

--- src/main/java/com/hwk9407/bookmanagementassignment/api/author/controller/AuthorController.java
+++ src/main/java/com/hwk9407/bookmanagementassignment/api/author/controller/AuthorController.java
@@ -3,15 +3,12 @@ package com.hwk9407.bookmanagementassignment.api.author.controller;
 import com.hwk9407.bookmanagementassignment.api.author.dto.request.AddAuthorRequest;
 import com.hwk9407.bookmanagementassignment.api.author.dto.response.AddAuthorResponse;
 import com.hwk9407.bookmanagementassignment.api.author.dto.response.RetrieveAllAuthorsResponse;
+import com.hwk9407.bookmanagementassignment.api.author.dto.response.RetrieveAuthorResponse;
 import com.hwk9407.bookmanagementassignment.api.author.service.AuthorService;
-import com.hwk9407.bookmanagementassignment.api.book.dto.response.RetrieveAllBooksResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -32,6 +29,14 @@ public class AuthorController {
     @GetMapping("/authors")
     public ResponseEntity<RetrieveAllAuthorsResponse> retrieveAllAuthors() { // todo: 페이지네이션 적용 필요
         RetrieveAllAuthorsResponse res = authorService.retrieveAllAuthors();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(res);
+    }
+
+    @GetMapping("/authors/{id}")
+    public ResponseEntity<RetrieveAuthorResponse> retrieveAuthor (@PathVariable Long id) {
+        RetrieveAuthorResponse res = authorService.retrieveAuthor(id);
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(res);
--- src/main/java/com/hwk9407/bookmanagementassignment/api/author/service/AuthorService.java
+++ src/main/java/com/hwk9407/bookmanagementassignment/api/author/service/AuthorService.java
@@ -3,9 +3,11 @@ package com.hwk9407.bookmanagementassignment.api.author.service;
 import com.hwk9407.bookmanagementassignment.api.author.dto.request.AddAuthorRequest;
 import com.hwk9407.bookmanagementassignment.api.author.dto.response.AddAuthorResponse;
 import com.hwk9407.bookmanagementassignment.api.author.dto.response.RetrieveAllAuthorsResponse;
+import com.hwk9407.bookmanagementassignment.api.author.dto.response.RetrieveAuthorResponse;
 import com.hwk9407.bookmanagementassignment.domain.author.Author;
 import com.hwk9407.bookmanagementassignment.domain.author.AuthorRepository;
 import com.hwk9407.bookmanagementassignment.exception.EmailAlreadyExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -34,5 +36,13 @@ public class AuthorService {
     public RetrieveAllAuthorsResponse retrieveAllAuthors() {
         List<Author> authors = authorRepository.findAll();
         return RetrieveAllAuthorsResponse.from(authors);
+    }
+
+    @Transactional(readOnly = true)
+    public RetrieveAuthorResponse retrieveAuthor(Long id) {
+        Author author = authorRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("조회되지 않는 저자 ID 입니다.")
+        );
+        return RetrieveAuthorResponse.from(author);
     }
 }
