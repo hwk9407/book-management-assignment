4522fddf3e069fd7e8b93b8cfd589d122d6f6549
hwk9407 <hwk__@naver.com>
2025-02-22 23:55:43 +0900

    [#5]feat: 저자 생성 API 비즈니스 로직 구현

--- src/main/java/com/hwk9407/bookmanagementassignment/api/author/service/AuthorService.java
+++ src/main/java/com/hwk9407/bookmanagementassignment/api/author/service/AuthorService.java
@@ -2,9 +2,12 @@ package com.hwk9407.bookmanagementassignment.api.author.service;
 
 import com.hwk9407.bookmanagementassignment.api.author.dto.request.AddAuthorRequest;
 import com.hwk9407.bookmanagementassignment.api.author.dto.response.AddAuthorResponse;
+import com.hwk9407.bookmanagementassignment.domain.author.Author;
 import com.hwk9407.bookmanagementassignment.domain.author.AuthorRepository;
+import com.hwk9407.bookmanagementassignment.exception.EmailAlreadyExistsException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,10 +15,15 @@ public class AuthorService {
 
     private final AuthorRepository authorRepository;
 
+    @Transactional
     public AddAuthorResponse addAuthor(AddAuthorRequest req) {
-        // DB에 이메일이 중복됐는지 확인
-        // 없으면 DB 저장
-        // id 값 Response Dto에 저장 후 반환
-        return null;
+        if (authorRepository.existsByEmail(req.email())) {
+            throw new EmailAlreadyExistsException("이미 존재하는 이메일입니다.");
+        }
+        Author author = authorRepository.save(Author.builder()
+                .name(req.name())
+                .email(req.email())
+                .build());
+        return new AddAuthorResponse(author.getId());
     }
 }
