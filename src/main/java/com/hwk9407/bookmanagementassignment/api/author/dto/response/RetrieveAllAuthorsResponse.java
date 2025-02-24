85061769badc6545f7af16bfac6929e468d952e6
hwk9407 <hwk__@naver.com>
2025-02-24 21:20:06 +0900

    [#8]feat: Book, Author 목록 조회 시 페이지네이션 적용 및 페이지 정보 필드 추가

--- src/main/java/com/hwk9407/bookmanagementassignment/api/author/dto/response/RetrieveAllAuthorsResponse.java
+++ src/main/java/com/hwk9407/bookmanagementassignment/api/author/dto/response/RetrieveAllAuthorsResponse.java
@@ -1,18 +1,26 @@
 package com.hwk9407.bookmanagementassignment.api.author.dto.response;
 
 import com.hwk9407.bookmanagementassignment.domain.author.Author;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public record RetrieveAllAuthorsResponse(
-        List<RetrieveAuthorResponse> contents
+        List<RetrieveAuthorResponse> contents,
+        int page,
+        int size,
+        int totalPage
 ) {
 
-    public static RetrieveAllAuthorsResponse from(List<Author> authors) {
+    public static RetrieveAllAuthorsResponse from(Page<Author> authors) {
         return new RetrieveAllAuthorsResponse(
-                authors.stream()
+                authors.getContent().stream()
                         .map(RetrieveAuthorResponse::from)
-                        .toList()
+                        .toList(),
+                authors.getNumber() + 1,
+                authors.getSize(),
+                authors.getTotalPages()
+
         );
     }
 }
